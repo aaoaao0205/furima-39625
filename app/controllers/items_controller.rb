cc4736a3983df8f5cd7494111333aa460232201a
aaoaao0205 <aaoaao0205@gmail.com>
2023-08-08 16:40:50 +0900

    画像の保存を許可するストロングパラメーターの設置

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,9 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :product_name, :description, :category_id, :condition_id, :shipping_fee_responsibility_id, :prefecture_id, :days_to_ship_id :price,).merge(user_id: current_user.id)
+  end
 end
--- db/migrate/20230808065047_create_items.rb
+++ db/migrate/20230808065047_create_items.rb
@@ -4,10 +4,10 @@ class CreateItems < ActiveRecord::Migration[7.0]
       t.string :product_name,                     null: false
       t.text :description,                        null: false
       t.integer :category_id,                     null: false
-      t.integer :condition_id ,                   null: false
-      t.integer :shipping_fee_responsibility_id , null: false
+      t.integer :condition_id,                   null: false
+      t.integer :shipping_fee_responsibility_id, null: false
       t.integer :prefecture_id,                   null: false
-      t.integer :price ,                          null: false
+      t.integer :price,                          null: false
       t.integer :days_to_ship_id,                 null: false
       t.references :user,                         null: false, foreign_key: true
 
