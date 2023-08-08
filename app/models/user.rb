c82a95eaaaf6c6f2de745b31773da58f3afc5d15
aaoaao0205 <aaoaao0205@gmail.com>
2023-08-08 12:49:16 +0900

    Rubocopの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
-
-   validates :nickname, presence: true
-   validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-   validates :kana_last_name, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-   validates :kana_first_name, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-   validates :birthday, presence: true 
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
 end
