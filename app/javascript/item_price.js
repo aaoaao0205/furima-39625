20430948b6d4d8a17b73ce367a14ff4c0945a5c9
aaoaao0205 <aaoaao0205@gmail.com>
2023-08-09 16:38:58 +0900

    HTMLをJavaScriptで作成

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,6 +2,13 @@ window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+    
+    const tax = Math.floor(inputValue * 0.1);
+    const profit = Math.floor(inputValue - tax);
+    
+    addTaxDom.innerHTML = tax;
+    profitDom.innerHTML = profit;
   });
 });
