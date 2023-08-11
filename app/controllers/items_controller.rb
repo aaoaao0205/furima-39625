8c17645e5e386f3c552aa9ea9df991783a05db47
aaoaao0205 <aaoaao0205@gmail.com>
2023-08-11 16:56:01 +0900

    renderとコントローラーの変更(URLへ直接アクセスすることを防ぐ、コードをまとめる）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,10 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_item, only: [:show, :edit, :update]
+  before_action :require_owner, only: [:edit, :update]
 
   def index
-    @items = Item.order('created_at DESC')
+    @items = Item.order(created_at: :desc)
   end
 
   def new
@@ -12,37 +14,41 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to '/'
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  #def item_order
-    #@item = Item.find(params[:id])
-    #return unless @item.order
-
-    #redirect_to '/'
-  #end
-
   def show
-    @item = Item.find(params[:id])
-
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to item_path(item)
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
+
   def item_params
     params.require(:item).permit(:image, :product_name, :description, :category_id, :condition_id,
-                                 :shipping_fee_responsibility_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+                                 :shipping_fee_responsibility_id, :prefecture_id, :days_to_ship_id, :price)
+          .merge(user_id: current_user.id)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
+  def require_owner
+    unless @item.user_id == current_user.id
+      redirect_to user_session_path
+    end
   end
 end
