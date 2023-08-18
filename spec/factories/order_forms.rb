2d835323b5183b811b04be357701f7f3ab53320e
aaoaao0205 <aaoaao0205@gmail.com>
2023-08-18 19:18:27 +0900

    商品購入機能(再修正)

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :order_form do
-    association :user
-    association :item
     postal_code { "#{rand(100..999)}-#{rand(1000..9999)}" }
     prefecture_id { Faker::Number.between(from: 1, to: 47) }
     city { Faker::Address.city }
